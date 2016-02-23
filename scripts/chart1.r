d4c30dc7e1a9c37a7f32325b0176815e592a7cd8
greycabb <greycabbage@gmail.com>
2016-02-23 00:17:30 -0800

    fixed HTML page

--- scripts/chart1.r
+++
@@ -1,6 +0,0 @@
-install.packages("plotly")
-library(plotly)
-data_main <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv")
-Scatter_Plot <- function(data_frame) {
-  return(plot_ly(data = data_frame,  xlab ="students", y = data_main$How.many.countries.have.you.visited.in.your.life., mode = "markers"))
-}
