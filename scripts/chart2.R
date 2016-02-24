2a39d5d0fdb679c002957db644ea8a99a133534c
Jacob Burke <Jacob@Jacobs-MacBook-Pro-3.local>
2016-02-24 01:40:28 -0800

    jacob edit

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -1,15 +1,10 @@
 library(plotly)
 library(dplyr)
 
-# Reading data (in the form of a data.frame) from the source
-data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv")
-
 Bar_Graph<- function(data_frame) {
-  
 # Rendering the second chart, a bar graph
 counts <- table(data_frame$What.is.your.current.class.standing.)
 return (barplot(counts, main="Class Distribution", 
         xlab="Number of Students per class", col=c("darkblue","red", "yellow", "green")))
 }
 
-Bar_Graph(data)
