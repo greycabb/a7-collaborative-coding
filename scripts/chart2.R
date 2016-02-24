4d309aeeef9edc2f8f5c27566315fb4a4f1344be
Jacob Burke <Jacob@D-69-91-153-161.dhcp4.washington.edu>
2016-02-24 12:47:10 -0800

    jacob edits

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -1,10 +1,13 @@
 library(plotly)
 library(dplyr)
 
+# Rendering the second chart, a bar graph
 Bar_Graph <- function(data_frame) {
   
-  # Rendering the second chart, a bar graph
+  #sets data into a variable
   counts <- table(data_frame$What.is.your.current.class.standing.)
+  
+  #returns bar graph
   return (barplot(counts, main="Class Distribution", 
         xlab="Number of Students per class", col=c("darkblue","red", "yellow", "green")))
 }
