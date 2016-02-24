2ce72b7a39f6b395148375872ccd34aada6135c4
Jonathan Jewik <Jonathan Jewik>
2016-02-23 18:20:33 -0800

    jon jewik

---
+++ scripts/chart1.r
@@ -0,0 +1,19 @@
+install.packages("plotly")
+library(plotly)
+
+Scatter_Plot <- function(data_frame) {
+  
+  x <- list(
+    title = "# Student"
+  )
+  y <- list(
+    title = "Countries visited"
+  )
+  
+  countries_visited <- data_frame$How.many.countries.have.you.visited.in.your.life.
+  return(plot_ly(data_frame,
+                 y = countries_visited,
+                 mode = "markers")
+  ) %>%
+    layout(xaxis = x, yaxis = y, title = "How many countries each student has visited")
+}
